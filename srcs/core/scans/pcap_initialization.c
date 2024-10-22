d0ce2f351361a1b3229acf00c878f3cff512538a
intrusions <xeloronkali@gmail.com>
2024-10-22 03:10:06 +0200

    fix(pcap-initialization): datarace fix

--- srcs/core/scans/pcap_initialization.c
+++ srcs/core/scans/pcap_initialization.c
@@ -2,17 +2,21 @@
 #include <pcap.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <pthread.h>
 
 bool pcap_initialization(pcap_t **handle)
 {
+    static pthread_mutex_t pcap_mutex = PTHREAD_MUTEX_INITIALIZER;
     char *devs;
     char err_buf[PCAP_ERRBUF_SIZE];
 
+    pthread_mutex_lock(&pcap_mutex);
     devs = pcap_lookupdev(err_buf);
     if (!devs) {
         fprintf(stderr, "pcap_lookupdev error: %s\n", err_buf);
         return false;
     }
+    pthread_mutex_unlock(&pcap_mutex);
 
     *handle = pcap_open_live(devs, BUFSIZ, 1, 200, err_buf);
     if (!*handle) {
